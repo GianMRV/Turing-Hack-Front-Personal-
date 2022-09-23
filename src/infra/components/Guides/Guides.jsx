84f0f5f3d4aeae503b5719f3949e8092495fc4c4
GianMRV <gira.208@gmail.com>
2022-09-23 08:32:28 -0400

    feat: add Guides.jsx file

--- src/infra/components/Guides/Guides.jsx
+++ src/infra/components/Guides/Guides.jsx
@@ -1,4 +1,5 @@
 import { Box, Flex, Heading, Image, Text } from "@chakra-ui/react";
+import { Link } from "react-router-dom";
 
 const trade = {
     url: 'https://i.pinimg.com/564x/96/40/d3/9640d3f7529fcd5ae5778591aabda45a.jpg'
@@ -13,45 +14,69 @@ const wallet = {
 function Guides(){
     return(
         <>
+            <Heading></Heading>
             <Flex
-                direction={'column'}
+                direction={{ base: 'column', md: 'row' }}
+                px={10}
+                py={10}
                 gap={2}>
-                <Flex
-                    direction={{ base: 'column' , md: 'row' }}
-                    alignItems={'center'}
-                    borderWidth={'2px'}>
-                    <Image src={trade.url} w={{ base: 'full', md: '40%'}}></Image>
-                    <Box px={5}>
-                        <Heading textAlign={'center'}>¿Cómo hacer Trading?</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde repudiandae laudantium doloremque nam in eos, ipsum vitae fugit tempore quasi soluta accusamus non id expedita, placeat velit autem architecto. Itaque?
-                        </Text>
+                <Link to='/signin'>
+                    <Box
+                        _hover={{
+                            shadow: 'outline',
+                            transform: 'scale(0.98)'
+                        }}
+                        shadow={'2xl'}
+                        direction={'column'}
+                        alignItems={'center'}
+                        borderWidth={'2px'}>
+                        <Image src={trade.url} w={'full'}></Image>
+                        <Box px={5}>
+                            <Heading textAlign={'center'}>Haz Trading</Heading>
+                            <Text textAlign={'left'}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde repudiandae laudantium doloremque nam in eos, ipsum vitae fugit tempore quasi soluta accusamus non id expedita, placeat velit autem architecto. Itaque?
+                            </Text>
+                        </Box>
                     </Box>
-                </Flex>
-                <Flex
-                    direction={{ base: 'column' , md: 'row-reverse' }}
-                    alignItems={'center'}
-                    borderWidth={'2px'}>
-                    <Image src={deposito.url} w={{ base: 'full', md: '40%'}}></Image>
-                    <Box px={5}>
-                        <Heading textAlign={'center'}>Depositar o pagar</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde repudiandae laudantium doloremque nam in eos, ipsum vitae fugit tempore quasi soluta accusamus non id expedita, placeat velit autem architecto. Itaque?
-                        </Text>
+                </Link>
+                <Link to='/login'>
+                    <Box
+                        _hover={{
+                            shadow: 'outline',
+                            transform: 'scale(0.98)'
+                        }}
+                        shadow={'2xl'}
+                        direction={'column'}
+                        alignItems={'center'}
+                        borderWidth={'2px'}>
+                        <Image src={deposito.url} w={'full'}></Image>
+                        <Box px={5}>
+                            <Heading textAlign={'center'}>Deposita o paga</Heading>
+                            <Text textAlign={'left'}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde repudiandae laudantium doloremque nam in eos, ipsum vitae fugit tempore quasi soluta accusamus non id expedita, placeat velit autem architecto. Itaque?
+                            </Text>
+                        </Box>
                     </Box>
-                </Flex>
-                <Flex
-                    direction={{ base: 'column' , md: 'row' }}
-                    alignItems={'center'}
-                    borderWidth={'2px'}>
-                    <Image src={wallet.url} w={{ base: 'full', md: '40%', lg: '36%'}}></Image>
-                    <Box px={5}>
-                        <Heading textAlign={'center'}>Configura tu Wallet</Heading>
-                        <Text>
-                            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde repudiandae laudantium doloremque nam in eos, ipsum vitae fugit tempore quasi soluta accusamus non id expedita, placeat velit autem architecto. Itaque?
-                        </Text>
+                </Link>
+                <Link to='/recover'>
+                    <Box
+                        _hover={{
+                            shadow: 'outline',
+                            transform: 'scale(0.98)'
+                        }}
+                        shadow={'2xl'}
+                        direction={'column'}
+                        alignItems={'center'}
+                        borderWidth={'2px'}>
+                        <Image src={wallet.url} w={'full'}></Image>
+                        <Box px={5}>
+                            <Heading textAlign={'center'}>Configura tu Wallet</Heading>
+                            <Text textAlign={'left'}>
+                                Lorem ipsum dolor sit amet consectetur, adipisicing elit. Unde repudiandae laudantium doloremque nam in eos, ipsum vitae fugit tempore quasi soluta accusamus non id expedita, placeat velit autem architecto. Itaque?
+                            </Text>
+                        </Box>
                     </Box>
-                </Flex>
+                </Link>
             </Flex>
         </>
     )
